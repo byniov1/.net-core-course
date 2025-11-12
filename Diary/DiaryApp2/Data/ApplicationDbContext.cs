0df92580f61d3eb4f503ca83ef3544fc057feec0
byniov1 <karol.nowak0490@protonmail.com>
2025-11-12 15:52:46 +0100

    feat: init commit

---
+++ Diary/DiaryApp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DiaryApp2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp2.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+    }
+}
---
+++ Diary/DiaryApp2/Models/DiaryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp2.Models
+{
+    public class DiaryEntry
+    {
+        //[Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Title { get; set; }
+        //public string? Title1 { get; set; }
+        //public required string Title2 { get; set; }
+        //public string Title3 { get; set; } = string.Empty;
+        public string Content { get; set; }
+        public DateTime Creation { get; set; }
+
+    }
+}
