85aaef2e12f67ed5abd787b7f99795ddc5b8e8ac
byniov1 <karol.nowak0490@protonmail.com>
2026-02-04 12:41:11 +0100

    feat: further work and test in one

--- Diary/DiaryApp2/Models/DiaryEntry.cs
+++ Diary/DiaryApp2/Models/DiaryEntry.cs
@@ -4,15 +4,22 @@ namespace DiaryApp2.Models
 {
     public class DiaryEntry
     {
-        //[Key]
         public int Id { get; set; }
+        // public int DiaryEntryId { get; set; }
+        //[Key]
+        // public int SomeOtherNameId { get; set; }
+        
         [Required]
-        public required string Title { get; set; }
+        public required string Title { get; set; } = string.Empty;
         //public string? Title1 { get; set; }
         //public required string Title2 { get; set; }
         //public string Title3 { get; set; } = string.Empty;
-        public string Content { get; set; }
-        public DateTime Creation { get; set; }
 
+        [Required] 
+        public required string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Creation { get; set; } = DateTime.Now;
+        // public DateTime Creation { get; set; }
     }
 }
